b4d4790566e6d5fd9785de614d10367595025982
HamishDrogemuller <79230862+HamishDrogemuller@users.noreply.github.com>
2022-08-14 20:52:28 +1200

    Working base model

--- game.py
+++ game.py
@@ -2,34 +2,36 @@
 Game activity for SDV602, playing with the functionality of pysimpleGUI 
 Hamash, mort 
 """
+# Add image to dictionary
+#figure was to apply radio button option
 import PySimpleGUI as sg
 
 # Define game states to declare locations and track interaction 
 game_state = 'Prison'
-game_places =   {'Prison':{'Description':'You are in a prison, you can escape or stay, enter your decision',
-                        'Escape':'Hallway','Stay':'End'},
-                'Hallway':{'Description':'You enter the hallway, to your left is a room with the door ajar, would you like to enter or continue down the hallway?',
-                        'Enter':'Room', 'Continue':'Guard'},
-                'End':{'Description':'You were caught',
-                        '':'Prison' },
-                'Room':{'Description':'You enter the room, there is a knife, pick it up?',
-                        'Yes':'Guard', 'No':'Guard'},
-                'Guard':{'Description':'You are confronted by a guard, use the knife? Attack the guard or continue running?',
-                        'Knife':'Grab keys', 'Attack':'Grab keys', 'Continue':'End'},
-                'Grab keys':{'Description':'Would you like to go through the main gate or the sewer?',
-                        'Main gate':'End', 'Sewer':'Landing pad'},
-                'Landing pad':{'Description':'You are on the landing pad, would you like to stow away on the ship or run into the forest?',
-                        'Stow away':'Ship', 'Forest':'Forest'},
-                'Ship':{'Description':'Ship lands on new planet, would you like to stay on the ship or live the remainder of your criminal life on this docile planet?',
-                        'Stay':'Travel', 'Planet':'Live'},
-                'Travel':{'Description':'You join a band of mercenaries',
-                        '':'Prison' },
-                'Live':{'Description':'You live a happy life and raise a happy criminal family',
-                        '':'Prison'},
-                'Forest':{'Description':'The guards are chasing you, run or hide?',
-                        'Run':'Cliff', 'Hide':'End'},
-                'Cliff':{'Description':'You have come to a cliff, would you like to jump or surrender?', 
-                        'Surrender':'End', 'Jump':'End'}
+game_places =   {'Prison':{'Description':'You are in a prison, you can escape(1)\n or stay(2), enter your decision',
+                        '1':'Hallway','2':'End'},
+                'Hallway':{'Description':'You enter the hallway, to your left is a room with the door ajar, would you like to enter(1) or continue down the hallway(2)?',
+                        '1':'Room', '2':'Guard'},
+                'End':{'Description':'You were caught, try again(1)',
+                        '1':'Prison' },
+                'Room':{'Description':'You enter the room, there is a knife, pick it up(1) or leave it(2)?',
+                        '1':'Guard', '2':'Guard'},
+                'Guard':{'Description':'You are confronted by a guard, use the knife(1)? Attack the guard with your hands(2) or continue running(3)?',
+                        '1':'Grab keys', '2':'Grab keys', '3':'End'},
+                'Grab keys':{'Description':'Would you like to go through the main gate(1) or the sewer(2)?',
+                        '1':'End', '2':'Landing pad'},
+                'Landing pad':{'Description':'You are on the landing pad, would you like to stow away on the ship(1) or run into the forest(2)?',
+                        '1':'Ship', '2':'Forest'},
+                'Ship':{'Description':'Ship lands on new planet, would you like to stay on the ship(1) or live the remainder of your criminal life on this docile planet(2)?',
+                        '1':'Travel', '2':'Live'},
+                'Travel':{'Description':'You join a band of mercenaries(1)',
+                        '1':'Prison' },
+                'Live':{'Description':'You live a happy life and raise a happy criminal family(1)',
+                        '1':'Prison'},
+                'Forest':{'Description':'The guards are chasing you, run(1) or hide(2)?',
+                        '1':'Cliff', '2':'End'},
+                'Cliff':{'Description':'You have come to a cliff, would you like to jump(1) or surrender(2)?', 
+                        '1':'End', '2':'End'}
 }
 
 # Define functions to happen during execution 
@@ -41,39 +43,40 @@ def display_current_place():
     
     return game_places[game_state]['Description']
 
-def make_a_window():
-    """
-    Creates a game window
-    """
-    sg.theme('Dark Blue 3')  # please make your windows 
-    prompt_input = [sg.Text('Enter your command',font='Any 14'),sg.Input(key='-IN-',size=(20,1),font='Any 14')]
-    buttons = [sg.Button('Enter',  bind_return_key=True), sg.Button('Exit'), sg.Radio('Permission Granted', "Option1", default=False),sg.Radio('Permission not Granted', "Option2", default=False),sg.Radio('Permission not Granted', "Option3", default=False)]
-    command_col = sg.Column([prompt_input,buttons],element_justification='r')
-    layout = [[sg.Text('Description:', size=(7,1), font='Any 14'), sg.Text(display_current_place(),size=(100,4), font='Any 14', key='-OUTPUT-')],
-             [command_col]]
-
-    return  sg.Window('Prison Game', layout, size=(800,500), resizable=True)
 def game_play(option):
-    """
-    Runs the game_play
-    """
-    global game_state
-    
+        """
+                Runs the game_play
 
-    game_place = game_places[game_state]
-    proposed_state = game_place[option]
-    if option == 1:
-        game_places[game_state]['Description']
-        return game_places[game_state]['Description']
-    elif option == 2:
-        game_state = proposed_state
-        return game_places[game_state]['Description']
-    elif option == 3:
-        return game_places[game_state]['Description']
-    else:
-        return 'Please select a single option'+game_places[game_state]['Description']
-    
+                Args:
+                direction string: _North or South
+
+                Returns:
+        string: the sory at the current place
+        """ 
+        global game_state
         
+        if option.lower() in '123': # is this a nasty check?
+                game_place = game_places[game_state]
+                proposed_state = game_place[option]
+        if proposed_state == '' :
+            return 'You can not go that way.\n'+game_places[game_state]['Description']
+        else :
+            game_state = proposed_state
+            return game_places[game_state]['Description']
+
+def make_a_window():
+        """
+        Creates a game window
+        """
+        sg.theme('Dark Blue 3')  # please make your windows
+        prompt_input = [sg.Text('Enter your command',font='Any 14'),sg.Input(key='-IN-',size=(20,1),font='Any 14')]
+        buttons = [sg.Button('Enter',  bind_return_key=True), sg.Button('Exit')]
+        #sg.Radio('Permission Granted', "Option1", default=False),sg.Radio('Permission not Granted', "Option2", default=False),sg.Radio('Permission not Granted', "Option3", default=False)
+        command_col = sg.Column([prompt_input,buttons],element_justification='r')
+        layout = [[sg.Text('Description:', size=(7,1), font='Any 14'), sg.Text(display_current_place(),size=(100,4), font='Any 14', key='-OUTPUT-')],
+        [command_col]]
+        return  sg.Window('Prison Game', layout, size=(800,500), resizable=True)
+
 # Define main function with while loop that continually revolves around the case of a button
 # being pressed and the value of radio buttons being selected 
 if __name__ == "__main__":
@@ -88,14 +91,14 @@ if __name__ == "__main__":
         if event == 'Exit' or event == sg.WIN_CLOSED:
             break
         if event ==  'Enter': 
-                if 'Option1'.lower() in values['-IN-'].lower():
-                    current_story = game_play('Option1')
+                if '1'.lower() in values['-IN-'].lower():
+                    current_story = game_play('1')
                     window['-OUTPUT-'].update(current_story)
-                elif 'Option2'.lower() in values['-IN-'].lower():
-                    current_story = game_play('Option2')
+                elif '2'.lower() in values['-IN-'].lower():
+                    current_story = game_play('2')
                     window['-OUTPUT-'].update(current_story)
-                elif 'Option3'.lower() in values['-IN-'].lower():
-                    current_story = game_play('Option3')
+                elif '3'.lower() in values['-IN-'].lower():
+                    current_story = game_play('3')
                     window['-OUTPUT-'].update(current_story)
                 pass
 
--- gameExample.py
+++ gameExample.py
@@ -7,9 +7,9 @@ import PySimpleGUI as sg
 game_state = 'Forest'
 game_places = {'Forest':{'Story':'You are in the forest.\nTo the north is a cave.\nTo the south is a castle',
                         'North':'Cave','South':'Castle'},
-              'Cave':{'Story':'You are at the cave.\nTo the south is forest.',
+              'Cave':{'Story':'You are at the cave. To the south is forest.',
                         'North':'','South':'Forest'},
-              'Castle':{'Story':'You are at the castle.\nTo the north is forest.',
+              'Castle':{'Story':'You are at the castle. To the north is forest.',
                         'North':'Forest','South':''},
                 }
 
@@ -35,20 +35,15 @@ def game_play(direction):
     """
     global game_state
     
-
-    game_place = game_places[game_state]
-    proposed_state = game_place[direction]
-    if Option1==True:
-        game_places[game_state]['Description']
-        return game_places[game_state]['Description']
-    elif Option2==True:
-        game_state = proposed_state
-        return game_places[game_state]['Description']
-    elif Option3==True:
-        return game_places[game_state]['Description']
-    else:
-        return 'Please select an option'+game_places[game_state]['Description']
-    
+    if direction.lower() in 'northsouth': # is this a nasty check?
+        game_place = game_places[game_state]
+        proposed_state = game_place[direction]
+        if proposed_state == '' :
+            return 'You can not go that way.\n'+game_places[game_state]['Story']
+        else :
+            game_state = proposed_state
+            return game_places[game_state]['Story']
+        
         
 def make_a_window():
     """
@@ -61,10 +56,10 @@ def make_a_window():
     prompt_input = [sg.Text('Enter your command',font='Any 14'),sg.Input(key='-IN-',size=(20,1),font='Any 14')]
     buttons = [sg.Button('Enter',  bind_return_key=True), sg.Button('Exit')]
     command_col = sg.Column([prompt_input,buttons],element_justification='r')
-    layout = [[sg.Text('DEscription:', size=(7,1), font='Any 14'), sg.Text(show_current_place(),size=(100,4), font='Any 14', key='-OUTPUT-')],
+    layout = [[sg.Text('Story:', size=(7,1), font='Any 14'), sg.Text(show_current_place(),size=(100,4), font='Any 14', key='-OUTPUT-')],
              [command_col]]
 
-    return  sg.Window('Prison Game', layout, size=(max))
+    return  sg.Window('Adventure Game', layout, size=(320,200))
     
 
 if __name__ == "__main__":
@@ -92,3 +87,7 @@ if __name__ == "__main__":
                 pass
              
     window.close()
+    
+    
+    
+    
